555604c42ba4e2f92d65e997e03f19009467da19
Breandan O'Shaughnessy <breandan@avrthing.com>
2012-04-08 11:09:10 -0500

    added some comments to RTC.h

---
+++ MCB/MCB/RTC.h
@@ -0,0 +1,5 @@
+/*
+Head file for RTC.c the RODLOGIC real time clock and scheduling file.  
+
+RTC.c keeps a real time clock, and runs the USB task every 500 us, it uses timer 1, timer1 cannot be used for anything else when using the standard MCB code distro.
+*/
