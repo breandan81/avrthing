6b86afe0005f47f87d7065b9a4bd17db4f97715a
Breandan O'Shaughnessy <breandan@avrthing.com>
2012-04-09 20:11:58 -0500

    RLBMaster should be working

--- MCB/MCB/RLBMaster.cpp
+++ MCB/MCB/RLBMaster.cpp
@@ -1,10 +1,81 @@
 #include "RLBMaster.h"
 
+//MY CONSTANTS
 #define CLEAR 0
 #define SENDING 1
 #define RECEIVING 2
 
-int sendCommand(char slave)
-{
+//FROM DATASHEET
+#define START 0x08
+#define MT_SLA_ACK 0x18
+#define MR_SLA_ACK 0x40
+#define MT_DATA_ACK 0X28
+#define MR_DATA_ACK 0X50
 
+
+
+int RLBBegin(char slave, char receive)
+{
+//first we have to send a start condition
+//I'm not going to set TWEN here, because it should already be enabled,
+//Instead I'll check for TWEN
+	if(TWCR & _BV(TWEN))
+	{
+	TWCR |= _BV(TWINT) | _BV(TWSTA);
+	}
+//wait for twint to be set again, this indicates start condition was sent
+// (set is logic 0, I hate that)
+
+	while(!(TWCR & _BV(TWINT)));
+//now we have to check the status register
+	if((TWSR & 0xF8) != START)
+	{
+		return -1;
+	}
+//now we can send the slave address 
+	TWDR = slave << 1 | (receive?1:0);//last bit is high if receiving
+	TWCR |= _BV(TWINT);
+//wait for it again 
+	while(!(TWCR & _BV(TWINT)));
+//check the status
+	if((TWSR & 0xF8) != (receive?MR_SLA_ACK:MT_SLA_ACK))
+	{
+		return -1;
+	}
+return 0;
+}
+int RLBsendByte(char me)
+{
+	TWDR = me;
+	TWCR |= _BV(TWINT);
+	//and wait for it
+	while(!(TWCR & _BV(TWINT)));
+	//check status
+	if((TWSR & 0xF8) != MT_DATA_ACK)
+	{
+		return -1;
+	}
+	return 0;
+}
+int RLBrcvByte()
+{
+	if(TWCR & _BV(TWINT))
+	{
+		return -2;	
+	}
+	if(TWSR & 0xf8 != MR_DATA_ACK)
+	{
+		return -1;
+	}
+	return (int)TWDR;
+}
+
+int RLBstop()
+{
+	TWCR |= _BV(TWINT) | _BV(TWSTO);
+}
+
+void RLBinit()
+{
+	TWBR = 48*8/MHZ;
 }
--- MCB/MCB/RLBMaster.h
+++ MCB/MCB/RLBMaster.h
@@ -3,12 +3,19 @@ Goal here is to define only the functions needed for a polling (no interrupts) T
 functionality.  There are a million ways this could be expanded to take advantage of multimaster, interrupts,
 etc.  The goal of RodLogic is KISS 
 */
+extern "C"
+{
+#include "MCB.h"
+}
 
-int sendCommand(char slave); //returns -1 if fails
-int rcvByte(); //if twint is set pulls the last byte out and clears it, otherwise returns -1
-int sendByte();//sends a Byte
-int sendStop();//ends transmission
-int rcvFrom();//begins a master receive
+#include <avr/io.h>
+#include <stdio.h>
+
+int RLBbegin(char slave, char receive); //returns -1 if fails
+int RLBrcvByte(); //if twint is set pulls the last byte out and clears it, otherwise returns -1
+int RLBsendByte();//sends a Byte
+int RLBstop();//ends transmission
+void RLBinit();
 
 
 char status = 0; 
