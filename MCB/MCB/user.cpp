0f2476a542a56f79306525c826622c36c10d16e2
Breandan O'Shaughnessy <breandan@avrthing.com>
2012-04-26 13:19:23 -0500

    ringbuffer tested, everything is awesum

--- MCB/MCB/user.cpp
+++ MCB/MCB/user.cpp
@@ -11,7 +11,17 @@ void  setup()
 }
 void loop()
 {
-	delayMillis(100);
-	printf("hello %d\n\r", cur);
-	cur++;
+	delayMillis(1000);
+
+	printf("pushing 0 through 10 into an 8 byte buffer, should bork it \n\r");
+	for(int i = 0; i < 11; i++)
+	{
+		me.push(i);
+	}
+	printf("popping it all out\n\r");
+	for(int i = 0; i < 11; i++)
+	{
+		printf("popped %d \n\r", me.pop());
+	}
+	
 }
