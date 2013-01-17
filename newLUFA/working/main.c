65f9138a56b05001dc5c1834061a4074d6d29b48
Rick Mangus <rickmangus@gmail.com>
2013-01-17 12:01:32 -0600

    New version with LUFA 20120730

---
+++ newLUFA/working/main.c
@@ -0,0 +1,35 @@
+#include "avrthing.h"
+
+/** Main program entry point. This routine contains the overall program flow, including initial
+ *  setup of all components and the main program loop.
+ */
+int main(void)
+{
+	SetupHardware();
+
+	sei();
+
+	for (;;)
+	{
+		CheckJoystickMovement();
+
+		/* Must throw away unused bytes from the host, or it will lock up while waiting for the device */
+		runUSB();
+		USB_USBTask();
+	}
+}
+
+/** Configures the board hardware and chip peripherals for the demo's functionality. */
+void SetupHardware(void)
+{
+	/* Disable watchdog if enabled by bootloader/fuses */
+	MCUSR &= ~(1 << WDRF);
+	wdt_disable();
+
+	/* Disable clock division */
+	clock_prescale_set(clock_div_1);
+
+	/* Hardware Initialization */
+	USB_Init();
+}
+
